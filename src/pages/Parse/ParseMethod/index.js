aa8441b42bf97738cab0cbbf13a7caaa3d0c5618
wuyx <wuyx@dongwu-inc.com>
2019-06-04 11:17:28 +0800

    add parse method

--- src/pages/Parse/ParseMethod/index.js
+++ src/pages/Parse/ParseMethod/index.js
@@ -220,6 +220,206 @@ class ParseMethod extends Component{
             &#125;<br/>
           </div>
         </div>
+        {/* 7 */}
+        <div
+          style={{marginTop:'30px'}}>
+          <p
+            style={{
+              letterSpacing:1,
+              fontSize:'22px',
+              color:'#1890FF'
+            }}>parseSearch</p>
+          <div
+            style={{fontSize:'13px',color:'#666',letterSpacing:1,padding:'20px',background:'#fff',borderRadius:'12px'}}>
+            /**<br/>
+            * 用户搜索功能<br/>
+            * @param &#123;需要传递的Parse对象&#125; a<br/>
+            * @param &#123;需要在那个class类下进行搜索填写当前的class类名&#125; b<br/> 
+            * @param &#123;输入查询的字段的值 Object&#125; c<br/>
+            * @param &#123;查询完成后的返回结果&#125; d<br/>
+            * @param &#123;是否开启模糊匹配 boolean 默认开启&#125; e<br/>
+            */<br/><br/>
+            function parseSearch(a,b,c,d,e=true)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let searchList = a.Object.extend(b)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let list = new a.Query(searchList)<br/>
+
+            &nbsp;&nbsp;&nbsp;&nbsp;if(e)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;for(let i in c)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;list.equalTo(i,&#123;"$regex":c[i]&#125;)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;else&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;for(let i in c)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;list.equalTo(i,&#123;"$regex":c[i]&#125;)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;list.equalTo(i,c[i])<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;list.find().then(res=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(res)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;).catch(err=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(err)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;)<br/>
+          &#125;<br/>
+          </div>
+        </div>
+        {/* 8 */}
+        <div
+          style={{marginTop:'30px'}}>
+          <p
+            style={{
+              letterSpacing:1,
+              fontSize:'22px',
+              color:'#1890FF'
+            }}>parseGetRelationData</p>
+          <div
+            style={{fontSize:'13px',color:'#666',letterSpacing:1,padding:'20px',background:'#fff',borderRadius:'12px'}}>
+            /**<br/>
+            * Relation 形式的数据查询<br/>
+            * @param &#123;需要查询带有relation格式数据的字段&#125; a<br/> 
+            * @param &#123;relation格式字段的名称&#125; b<br/>
+            * @param &#123;查询成功或者失败的回调函数&#125; c<br/>
+            * @param &#123;增加条件查询&#125; d<br/>
+            */<br/><br/>
+            function parseGetRelationData(a,b,c,d='')&#123;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;let relationData = a.relation(b)<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;if(d==='')&#123;&#125;else&#123;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;for(let i in d)&#123;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;relationData.query().equalTo(i,d[i])<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;relationData.query().find().then(req=>&#123;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c(req)<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;).catch(err=>&#123;<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c(err)<br/> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;)<br/> 
+            &#125;<br/> 
+          </div>
+        </div>
+        {/* 9 */}
+        <div
+          style={{marginTop:'30px'}}>
+          <p
+            style={{
+              letterSpacing:1,
+              fontSize:'22px',
+              color:'#1890FF'
+            }}>parseFileSave</p>
+          <div
+            style={{fontSize:'13px',color:'#666',letterSpacing:1,padding:'20px',background:'#fff',borderRadius:'12px'}}>
+            /**<br/>
+            * 存储图片<br/>
+            * @param &#123;需要传入的Parse对象&#125; a<br/> 
+            * @param &#123;存储图片的字段名称 String&#125; b<br/>
+            * @param &#123;需要传入的base64图片&#125; c<br/>
+            * @param &#123;图片存储成功后的回调函数&#125; d<br/>
+            */<br/><br/>
+            function parseFileSave(a,b,c,d)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let file = new a.File(b, &#123; base64: c &#125;)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;file.save().then(req=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(req)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;).catch(err=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(err)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;)<br/>
+            &#125; <br/>
+          </div>
+        </div>
+        {/* 10 */}
+        <div
+          style={{marginTop:'30px'}}>
+          <p
+            style={{
+              letterSpacing:1,
+              fontSize:'22px',
+              color:'#1890FF'
+            }}>parseRelationAdd</p>
+          <div
+            style={{fontSize:'13px',color:'#666',letterSpacing:1,padding:'20px',background:'#fff',borderRadius:'12px'}}>
+            /**<br/>
+            * Relation格式的数据的增加<br/>
+            * @param &#123;带有relation数据结构的对象&#125; a<br/> 
+            * @param &#123;带有relation数据结构的对象的字段名称&#125; b<br/>
+            * @param &#123;需要添加的对象，可以是单个对象或者是数组对象&#125; c<br/>
+            * @param &#123;添加成功或失败后的回调函数&#125; d<br/>
+            */<br/><br/>
+            function parseRelationAdd(a,b,c,d)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let relation = a.relation(b)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;relation.add(c)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;a.save().then(req=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(req)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;).catch(err=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(err)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;)<br/>
+            &#125;<br/>
+          </div>
+        </div>
+        {/* 11 */}
+        <div
+          style={{marginTop:'30px'}}>
+          <p
+            style={{
+              letterSpacing:1,
+              fontSize:'22px',
+              color:'#1890FF'
+            }}>parseRelationRemove</p>
+          <div
+            style={{fontSize:'13px',color:'#666',letterSpacing:1,padding:'20px',background:'#fff',borderRadius:'12px'}}>
+            /**<br/>
+            * Relation格式的数据的删除<br/>
+            * @param &#123;带有relation数据结构的对象&#125; a<br/> 
+            * @param &#123;带有relation数据结构的对象的字段名称&#125; b<br/>
+            * @param &#123;需要删除的对象，可以是单个对象或者是数组对象&#125; c<br/>
+            * @param &#123;删除成功或失败后的回调函数&#125; d<br/>
+            */<br/><br/>
+            function parseRelationRemove(a,b,c,d)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let relation = a.relation(b)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;relation.remove(c)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;a.save().then(req=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(req)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;).catch(err=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;d(err)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;)<br/>
+            &#125;<br/>
+          </div>
+        </div>
+        {/* 12 */}
+        <div
+          style={{marginTop:'30px'}}>
+          <p
+            style={{
+              letterSpacing:1,
+              fontSize:'22px',
+              color:'#1890FF'
+            }}>parsePagingQuery</p>
+          <div
+            style={{fontSize:'13px',color:'#666',letterSpacing:1,padding:'20px',background:'#fff',borderRadius:'12px'}}>
+            /**<br/>
+            * Parse分页效果查询，用来做分页<br/>
+            * @param &#123;需要传入的Parse对象&#125; a<br/> 
+            * @param &#123;需要查询的class类名&#125; b<br/>
+            * @param &#123;跳过的条数&#125; c<br/>
+            * @param &#123;限制输出的条数&#125; d<br/>
+            * @param &#123;查询成功或者失败的回调函数&#125; e<br/>
+            * @param &#123;指定返回指定的字段，数组格式，里面包含想要查询的字段名称&#125; f<br/>
+            */<br/><br/>
+            function parsePagingQuery(a,b,c,d,e,f='')&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let DataList = a.Object.extend(b)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;let queryData = new a.Query(DataList)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;if(f==='')&#123;&#125;else&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;for(let i=0;i&lt;f.length;i++)&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;queryData.select(f[i])<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;queryData.skip(c)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;queryData.limit(d)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;queryData.find().then(req=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e(req)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;).catch(err=>&#123;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e(err)<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&#125;)<br/>
+            &#125;<br/>
+          </div>
+        </div>
+
+
             
 
       </div>
